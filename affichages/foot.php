0157b0afaef4f1c225a1c5bf5a6061f68f8a616f
Calmes <christophe.calmes@laplateforme.io>
2024-02-23 09:07:44 +0100

    footer guyajeux

--- affichages/foot.php
+++ affichages/foot.php
@@ -1,8 +1,10 @@
 
-    <footer >
-  <div>
+    <footer class="flex-rows">
+      <nav><a href="https://guyajeux.salon/boutique/">Eshop Guyajeux</a></nav>
+  <div class="centrale">
       Copyrigth &copy; <?=date('Y')?> &nbsp;
   </div>
+  <aside class="adress">124 Bd Jean Jaurès,<br/> 13300 Salon-de-Provence<br/> téléphone: 06 59 64 95 42<br/> email: sebastienguyajeux@gmail.com</aside>
     </footer>
    </body>
  </html>
