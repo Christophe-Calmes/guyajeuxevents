b261a376eae9dad12ff1c86ada4fe40031e2c0ef
Calmes <christophe.calmes@laplateforme.io>
2024-03-05 11:37:32 +0100

    refactoring template

--- sources/news/objects/TemplatesArticles.php
+++ sources/news/objects/TemplatesArticles.php
@@ -8,22 +8,24 @@ Class TemplateArticle extends SQLAcessNews {
     {
         $this->pictureDirectory = 'sources/pictures/picturesNews/';
     }
+    protected function presentationText($data, $class) {
+        $result = listHTML($data, $class);
+        $result = lineBreak($result);
+        $result = strongHTML($result);
+        return $result;
+    }
     public function displayLastArticle() {
         $dataLastArticle=$this->selectLastArticle();
-        $finalText = listHTML($dataLastArticle[0]['text'], 'listClass');
-        $finalText = lineBreak($finalText);
-        $finalText = strongHTML($finalText);
+        $finalText = $this->presentationText($dataLastArticle[0]['text'], 'listClass');
         if($dataLastArticle) {
             echo'<div class="indexArticle">
-            <div class="TitleNews"><h3 class="subTitleSite">'.$dataLastArticle[0]['title'].'</h3>
-                    
+            <div class="TitleNews"><h3 class="subTitleSite">'.$dataLastArticle[0]['title'].'</h3>   
             </div>
                     <div class="pictureNews"><img class="imgNews" src="'.$this->pictureDirectory.$dataLastArticle[0]['picture'].'" alt="'.$dataLastArticle[0]['title'].'"/></div>
                     <div class="textNews">
                     <p class="dateNews">Le '.brassageDate($dataLastArticle[0]['creat_date']).'</p>
                     <p class="styleListNews">'.$finalText.'</p>
                     </div>
-              
                 </div>';
         } else {
             echo 'No article';
