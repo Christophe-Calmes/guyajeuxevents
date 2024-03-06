68882b3506bec4ccd8ee8a2061252603de6ee5b9
Calmes <christophe.calmes@laplateforme.io>
2024-03-06 16:25:47 +0100

    Présentation des événements, ajout du menu pour visualiser les événements publiques

--- sources/magasinEvents/objects/TemplateEvents.php
+++ sources/magasinEvents/objects/TemplateEvents.php
@@ -1,16 +1,24 @@
 <?php
 require('sqlEvents.php');
 require ('functions/functionDateTime.php');
+require('functions/functionPresentationText.php');
 Class TemplateEvents extends SQLEvents{
+    protected function presentationText($data, $class) {
+        $result = listHTML($data, $class);
+        $result = lineBreak($result);
+        $result = strongHTML($result);
+        return $result;
+    }
     private function templateEvent($variable) {
         $picturePath='sources/pictures/picturesEvents/';
         echo '<div class="gallery">';
         foreach ($variable as $value) {
+            $finalText = $this->presentationText($value['description'], 'listClass');
             echo '<div class="item">';
                 echo'<h2 class="subTitleSite titleEventItem">'.$value['title'].'</h2>';
                 echo '<article>';
                     echo '<p class="bold">Le '.brassageDate($value['dateEvent']).'</p>';
-                    echo '<p class="textNews">'.$value['description'].'</p>';
+                    echo '<p class="textNews">'.$finalText.'</p>';
                     echo '<p>Nombre de participant max : '.$value['numberMax'].' personnes</p>';
                     echo '<p>Participation aux frais : '.$value['contribution'].' €</p>';
                 echo '</article>';
