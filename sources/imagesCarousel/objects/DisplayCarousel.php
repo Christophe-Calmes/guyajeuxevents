27b6b845f2343087d13d313f0dcfbe0ca8839079
Calmes <christophe.calmes@laplateforme.io>
2024-04-16 11:39:40 +0200

    picture management corrections displayCarousel

--- sources/imagesCarousel/objects/DisplayCarousel.php
+++ sources/imagesCarousel/objects/DisplayCarousel.php
@@ -2,9 +2,11 @@
 require('sources/imagesCarousel/objects/SQLCarousel.php');
 Class displayCarousel extends SQLCarousel {
     private $picturePath;
+    private $yes;
     public function __construct()
     {
         $this->picturePath = 'sources/pictures/picturesCarousel/';
+        $this->yes = ['non', 'oui'];
     }
     public function displayPicture ($premier, $parPage, $idNav) {
         $dataPicture = $this->dataAdminPicture ($premier, $parPage);
@@ -23,24 +25,28 @@ Class displayCarousel extends SQLCarousel {
                         echo '<p>Date modification : '.brassageDate($value['date_update']).'</p>';
                         echo '<p>Legende associé : '.$value['legend'].'</p>';
                         echo '<img class="imgNews" src="'.$this->picturePath.$value['pictureName'].'" alt="'.$value['pictureName'].'"/>';
-                        echo '<form action="'.encodeRoutage(61).'" method="post">
-                        <input type="hidden" name="id" value="'.$value['id'].'"/>
-                        <button class="buttonForm red" type="submit" name="idNav" value="'.$idNav.'">Effacer</button>
-                        </form>';
                         echo '<form class="flex-colonne-form" action="'.encodeRoutage(62).'" method="post">
-                        <label for="orderPicture">Ordre d\'apparition ?</label>
-                        <input id="orderPicture" type="number" name="orderPicture" value="'.$value['orderPicture'].'" mini="0" max="124"/>
-                        <label for="legend">Légende de l\'image ?</label>
-                        <input type="text" id="legend" name="legend" value="'.$value['legend'].'"/>
-                        <label for="valid">Image valide ?</label>
-                        <select name="valid">
-                            <option value="1">Oui</option>
-                            <option value="0">Non</option>
-                        </select>
-                        <input type="hidden" name="id" value="'.$value['id'].'"/>
-                        <button class="buttonForm" type="submit" name="idNav" value="'.$idNav.'">Modifier</button>
+                            <label for="orderPicture">Ordre d\'apparition ?</label>
+                            <input id="orderPicture" type="number" name="orderPicture" value="'.$value['orderPicture'].'" mini="0" max="124"/>
+                            <label for="legend">Légende de l\'image ?</label>
+                            <input type="text" id="legend" name="legend" value="'.$value['legend'].'"/>
+                            <label for="valid">Image valide ?</label>
+                            <select name="valid">';
+                                for ($i=0; $i < count($this->yes); $i++) { 
+                                    if($value['valid'] == $i) {
+                                        echo '<option value="'.$i.'" selected>'.$this->yes[$value['valid']].'</option>';
+                                    } else {
+                                        echo '<option value="'.$i.'">'.$this->yes[$i].'</option>';
+                                    }
+                                }
+                        echo'</select>
+                            <input type="hidden" name="id" value="'.$value['id'].'"/>
+                            <button class="buttonForm" type="submit" name="idNav" value="'.$idNav.'">Modifier</button>
                         </form>';
-
+                    echo '<form action="'.encodeRoutage(61).'" method="post">
+                            <input type="hidden" name="id" value="'.$value['id'].'"/>
+                            <button class="buttonForm red" type="submit" name="idNav" value="'.$idNav.'">Effacer</button>
+                        </form>';
                     echo'</article>';
                 echo '</div>';
             }
